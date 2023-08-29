7ed8dbe230bfcfbc497d0b5a85364d04ec8eec07
Aluno <aluno@edu.senac.br>
2023-08-28 21:41:44 -0300

    meu primeiro commit

---
+++ js/Handson03.js
@@ -0,0 +1,9 @@
+function somar(){
+    /*entrada*/
+    var num1 = document.getElementById("num1").value
+    var num2 = document.getElementById("num2").value
+    /*processamento*/
+    var soma = Number.parseInt(num1)+Number.parseInt(numb)
+    /*saida*/
+    alert(num1+num2)
+}
